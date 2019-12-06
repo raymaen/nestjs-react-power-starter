25ea43045b32ea21b2ae8760d0b81e09629419a7
Eylon Harari <eylon.harari@sargon.com.au>
2019-12-06 18:09:26 +1100

    first

---
+++ server/public/precache-manifest.f0f4b9c600190d7194c63277f0e74d6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed052b6d0d45b466af126b18af62d9de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4362a4f312606a7c4f0e",
+    "url": "/static/js/2.d85fda0a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d85fda0a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0626b75bb4387580536b",
+    "url": "/static/js/main.c8aaf31e.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
